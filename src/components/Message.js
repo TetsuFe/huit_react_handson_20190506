06b71d5b1676e5b21991612c56da5a265d3873b1
TetsuFe <tetsubeg1ner27@gmail.com>
2019-05-01 07:28:45 +0900

    Add message style

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,39 @@
 import React from "react";
 
 export default class Message extends React.Component {
+
     render() {
+        const style = {
+            "item": {
+                display: "flex",
+                alignItems: "flex-end",
+                marginBottom: "0.8em",
+            },
+            "itemImage": {
+                width: "100px",
+                objectFit: "contain",
+                borderRadius: "20px",
+                verticalAlign: "top",
+            },
+            "itemName": {
+                fontSize: "75%"
+            },
+            "itemMessage": {
+                position: "relative",
+                display: "inline - block",
+                padding: "0.8em",
+                background: "#deefe8",
+                borderRadius: "4px",
+                lineHeight: "1.2em",
+            }
+        }
+
         return (
-            <div className="Message">
-                <img className="" src={this.props.message.profile_image} />
+            <div className="Message" style={style.item}>
+                <img style={style.itemImage} src={this.props.message.profile_image} />
                 <div className="">
-                    <p className="">@{this.props.message.user_name}</p>
-                    <p className="">{this.props.message.text}</p>
+                    <p style={style.itemName}>@{this.props.message.user_name}</p>
+                    <p style={style.itemMessage}>{this.props.message.text}</p>
                 </div>
             </div>
         );
